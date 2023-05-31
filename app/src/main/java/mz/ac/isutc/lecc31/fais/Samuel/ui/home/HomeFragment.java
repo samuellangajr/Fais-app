b10ae7f721baa204d00563f0902a19c052809e93
samuellangajr <samuellangajr@gmail.com>
2023-05-31 11:55:10 +0200

    first commit

---
+++ app/src/main/java/mz/ac/isutc/lecc31/fais/Samuel/ui/home/HomeFragment.java
@@ -0,0 +1,31 @@
+package mz.ac.isutc.lecc31.fais.Samuel.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import mz.ac.isutc.lecc31.fais.Samuel.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+
+
+
+}
