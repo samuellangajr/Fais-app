a93ef4b03e8c3d7dd51b47a4db854707919a3b0e
samuellangajr <samuellangajr@gmail.com>
2023-06-01 11:40:02 +0200

    Loging and signup function

--- app/src/main/java/mz/ac/isutc/lecc31/fais/Samuel/ui/create_account/Create_accountFragment.java
+++ app/src/main/java/mz/ac/isutc/lecc31/fais/Samuel/ui/create_account/Create_accountFragment.java
@@ -2,23 +2,111 @@ package mz.ac.isutc.lecc31.fais.Samuel.ui.create_account;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import mz.ac.isutc.lecc31.fais.Samuel.R;
 import mz.ac.isutc.lecc31.fais.Samuel.databinding.FragmentCreateAccountBinding;
 
 public class Create_accountFragment extends Fragment {
-private FragmentCreateAccountBinding createAccountBinding;
+private FragmentCreateAccountBinding  rbinding;
+private  boolean valid=true;
+private  boolean senha=true;
 
+private FirebaseAuth auth;
+private FirebaseFirestore firestore;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        createAccountBinding = FragmentCreateAccountBinding.inflate(getLayoutInflater());
+         rbinding = FragmentCreateAccountBinding.inflate(getLayoutInflater());
 
-        return createAccountBinding.getRoot();
+        //Inicializacao
+        auth=FirebaseAuth.getInstance();
+        firestore=FirebaseFirestore.getInstance();
+
+        //Registro
+        rbinding.criarConta.setOnClickListener(v -> {
+            String username=rbinding.nome.getText().toString();
+            String email=rbinding.email.getText().toString();
+            String pass=rbinding.password.getText().toString();
+            String pass2=rbinding.repetirPassword.getText().toString();
+            String perfil=String.valueOf(rbinding.tipoConta.getCheckedRadioButtonId());
+
+            verificarCampo(rbinding.nome);
+            verificarCampo(rbinding.email);
+            verificarCampo(rbinding.password);
+            verificarCampo(rbinding.repetirPassword);
+            verificarSenha(rbinding.password,rbinding.repetirPassword);
+
+            if (valid && senha){
+                //Start the user registration process
+
+                auth.createUserWithEmailAndPassword(email,pass).addOnSuccessListener(authResult -> {
+                    FirebaseUser user=auth.getCurrentUser();
+                    Toast.makeText(getContext(),"Conta criada com sucesso",Toast.LENGTH_LONG).show();
+
+                    //Create collections
+                    //assert user != null;
+                    DocumentReference df=firestore.collection("Users").document(user.getUid());
+                    Map<String,Object> userInfo= new HashMap<>();
+                    userInfo.put("Username",username);
+                    userInfo.put("UserEmail",email);
+                    //Specify if the user is Admin
+                    if(perfil.equals("0")){
+                        userInfo.put("UserLevel","promotor");
+                    } else {
+                        userInfo.put("UserLevel","comprador");
+                    }
+                    df.set(userInfo);
+                    LimparCampo();
+
+                }).addOnFailureListener(e -> {
+                    Toast.makeText(getContext(),"Failed to Create Account",Toast.LENGTH_LONG).show();
+                });
+
+            }
+
+
+
+        });
+
+        return  rbinding.getRoot();
+    }
+    private void verificarCampo(@NonNull EditText editText){
+        if(editText.getText().toString().isEmpty()){
+            editText.setError("Preencha o campo");
+            valid=false;
+        }else{
+            valid=true;
+        }
+    }
+    private void verificarSenha(@NonNull EditText editText1, EditText editText2){
+        if(editText1.getText().toString().equals(editText2.getText().toString())){
+            senha=true;
+        }else{
+            editText1.setError("A senha não é igual");
+            senha=false;
+        }
+    }
+    private void LimparCampo(){
+        rbinding.nome.setText("");
+        rbinding.email.setText("");
+        rbinding.password.setText("");
+        rbinding.repetirPassword.setText("");
     }
 }
--- app/src/main/java/mz/ac/isutc/lecc31/fais/Samuel/ui/login/LoginFragment.java
+++ app/src/main/java/mz/ac/isutc/lecc31/fais/Samuel/ui/login/LoginFragment.java
@@ -3,6 +3,7 @@ package mz.ac.isutc.lecc31.fais.Samuel.ui.login;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -10,33 +11,92 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+import mz.ac.isutc.lecc31.fais.Samuel.MainActivity;
 import mz.ac.isutc.lecc31.fais.Samuel.R;
 import mz.ac.isutc.lecc31.fais.Samuel.databinding.FragmentLoginBinding;
-import mz.ac.isutc.lecc31.fais.Samuel.ui.create_account.Create_accountFragment;
 
 
 public class LoginFragment extends Fragment {
-    private FragmentLoginBinding loginBinding;
+    private FragmentLoginBinding lbinding;
+    private boolean valid=true;
+    private FirebaseAuth auth;
+    private FirebaseFirestore firestore;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        loginBinding = FragmentLoginBinding.inflate(getLayoutInflater());
+        lbinding = FragmentLoginBinding.inflate(getLayoutInflater());
+        //Inicializando
+        auth=FirebaseAuth.getInstance();
+        firestore=FirebaseFirestore.getInstance();
 
-        loginBinding.navCreateAccount.setOnClickListener(new View.OnClickListener() {
+        lbinding.navCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
                 navController.navigate(R.id.nav_create_account);
             }
         });
-    loginBinding.entrar.setOnClickListener(new View.OnClickListener() {
+    lbinding.entrar.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
-            navController.navigate(R.id.nav_home_promotor);
+            String email=lbinding.email.getText().toString();
+            String pass=lbinding.password.getText().toString();
+            verificarCampo(lbinding.email);
+            verificarCampo(lbinding.password);
+
+            if (valid){
+                auth.signInWithEmailAndPassword(email,pass).addOnSuccessListener(authResult -> {
+                    Toast.makeText(getContext(),"Logged Successfully",Toast.LENGTH_SHORT).show();
+                    checkUserAccessLevel(Objects.requireNonNull(authResult.getUser()).getUid());
+
+                }).addOnFailureListener(e -> Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_LONG).show());
+
+
+            }
         }
     });
-        return loginBinding.getRoot();
+        return lbinding.getRoot();
     }
+
+    private void checkUserAccessLevel(String uid) {
+        DocumentReference df =firestore.collection("Users").document(uid);
+        //Extract Data from the document
+        df.get().addOnSuccessListener(documentSnapshot -> {
+            //Log.d("TAG","Sucesso"+documentSnapshot.getData());
+            //Identificando o perfil
+            if (Objects.equals(documentSnapshot.getString("UserLevel"), "promotor")){
+                // user is promotor
+                NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
+                navController.navigate(R.id.nav_home_promotor);
+
+            }else if (Objects.equals(documentSnapshot.getString("UserLevel"), "comprador")){
+                // user is comprador
+                NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
+                navController.navigate(R.id.nav_dashboard_participante);;
+
+            }
+
+        });
+
+    }
+
+    private void verificarCampo(@NonNull EditText editText){
+        if(editText.getText().toString().isEmpty()){
+            editText.setError("Preenca o campo");
+            valid=false;
+        }else{
+            valid=true;
+        }
+    }
+
 }
